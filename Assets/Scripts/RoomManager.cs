41e8f8e013938aa6eb08e6806b1e8e1d8ad3eda7
Dinçer Can Elitok <dincercanelitok@gmail.com>
2024-07-12 04:48:20 +0300

    PunAdded

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting..");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("connected to server");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test",null,null);
+        Debug.Log("in room");
+    }
+}
