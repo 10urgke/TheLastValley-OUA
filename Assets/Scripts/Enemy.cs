9139547995b12395a051e533d2779b7ffe6547ba
Dinçer Can Elitok <dincercanelitok@gmail.com>
2024-07-15 16:16:00 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -103,7 +103,7 @@ public class Enemy : MonoBehaviourPun
         }   
         else
         {
-            if(!animManager.IsInState("GetHit") || animManager.IsInState("Attack"))
+            if(!animManager.IsInState("GetHit") || !animManager.IsInState("Attack"))
                 animManager.SetTrigger("GetHit");                  
         }
             
